0d0f0123fdaee6dd0a1391e15d2111a3a73772e1
Sadık Ahmet <sadikahmetozdemir@outlook.com>
2022-04-19 16:55:14 +0300

    add foldering

---
+++ app/src/main/java/com/sadikahmetozdemir/notezz/ui/home/folder/HomeFolderFragment.kt
@@ -0,0 +1,14 @@
+package com.sadikahmetozdemir.notezz.ui.home.folder
+
+import com.sadikahmetozdemir.notezz.R
+import com.sadikahmetozdemir.notezz.base.BaseFragment
+import com.sadikahmetozdemir.notezz.databinding.FragmentHomeFolderBinding
+
+class HomeFolderFragment :
+    BaseFragment<FragmentHomeFolderBinding, HomeFolderViewModel>(R.layout.fragment_home_folder)
+
+
+
+
+
+
---
+++ app/src/main/java/com/sadikahmetozdemir/notezz/ui/home/folder/HomeFolderViewModel.kt
@@ -0,0 +1,6 @@
+package com.sadikahmetozdemir.notezz.ui.home.folder
+
+import com.sadikahmetozdemir.notezz.base.BaseViewModel
+
+class HomeFolderViewModel:BaseViewModel() {
+}
