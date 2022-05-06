607031fea40cbdd8aed603eeb62d85bcc89dde68
Scoto <scotoglu@gmail.com>
2022-05-06 12:14:27 +0300

    Add ForeignKey, edit arguments

--- app/src/main/java/com/sadikahmetozdemir/notezz/data/local/dto/NotesDatabase.kt
+++ app/src/main/java/com/sadikahmetozdemir/notezz/data/local/dto/NotesDatabase.kt
@@ -1,16 +1,28 @@
 package com.sadikahmetozdemir.notezz.data.local.dto
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.ForeignKey.CASCADE
 import androidx.room.PrimaryKey
 import java.io.Serializable
 
-@Entity(tableName = "notes")
+@Entity(
+    tableName = "notes",
+    foreignKeys = [ForeignKey(
+        entity = FolderDataBase::class,
+        parentColumns = ["folder_id"],
+        childColumns = ["id"],
+        onDelete = CASCADE
+    )]
+)
 data class NotesDatabase(
     var data: String?,
     var date: String?,
     var characters: Long?,
     var image: String? = null,
-    var folder: FolderDataBase
+    @ColumnInfo(name = "folder_id")
+    var folderId: Int
 ) : Serializable {
 
     @PrimaryKey(autoGenerate = true)
--- app/src/main/java/com/sadikahmetozdemir/notezz/service/dao/NotesDao.kt
+++ app/src/main/java/com/sadikahmetozdemir/notezz/service/dao/NotesDao.kt
@@ -1,12 +1,7 @@
 package com.sadikahmetozdemir.notezz.service.dao
 
 import androidx.lifecycle.LiveData
-import androidx.room.Dao
-import androidx.room.Delete
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
-import androidx.room.Query
-import androidx.room.Update
+import androidx.room.*
 import com.sadikahmetozdemir.notezz.data.local.dto.NotesDatabase
 
 @Dao
@@ -23,6 +18,9 @@ interface NotesDao {
 
     @Query("SELECT * FROM notes WHERE data LIKE :data")
     fun search(data: String): LiveData<List<NotesDatabase>>
+
+    @Query("select * from notes where folder_id=:folderId")
+    suspend fun getNotesByFolder(folderId: Int): List<NotesDatabase>
 
     @Delete
     suspend fun delete(note: NotesDatabase)
